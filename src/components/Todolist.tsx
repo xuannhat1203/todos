c21b62c537c15682a53415bace2e5c096e35edb8
xuannhat1203 <141288976+xuannhat1203@users.noreply.github.com>
2024-05-13 09:45:21 +0700

    Update Todolist.tsx

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -7,31 +7,18 @@ type JobType = {
   name: string;
   status: boolean;
 };
-
 export default function TodoList() {
   //#region Danh sách các State của component
   const [inputValue, setInputValue] = useState<string>("");
   const [showError, setShowError] = useState<boolean>(false);
   const [listJob, setListJob] = useState<JobType[]>(() => {
     const jobLocal = localStorage.getItem("jobs");
-
     // Nếu có jobLocal sẽ tiến hành ép kiểu nó về dạng JS , nếu không có sẽ mặc định là một []
     const jobs = jobLocal ? JSON.parse(jobLocal) : [];
-
     return jobs;
   });
   const [typeButton, setTypeButton] = useState<string>("add");
   const [idUpdate, setIdUpdate] = useState<string>("");
-
-  //#endregion
-
-  //#region Chứa danh sách các hàm của component
-
-  /**
-   * Hàm lấy giá trị trong ô input và validate dữ liệu đầu vào
-   * @param e Thông số chi tiết của sự kiện
-   * Auth: NVQUY (12/05/2024)
-   */
   const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
     // Cập nhật lại trị của State
     setInputValue(e.target.value);
@@ -43,25 +30,12 @@ export default function TodoList() {
       setShowError(true);
     }
   };
-
-  /**
-   * Hàm lưu dữ liệu lên localStorage
-   * @param key Key của dữ liệu trên local
-   * @param value Value của dữ liệu
-   *  Auth: NVQUY (12/05/2024)
-   */
   const saveData = (key: string, value: any) => {
     // Lưu dữ liệu lên local
     localStorage.setItem(key, JSON.stringify(value));
-
     // Cập nhật State để component được re-render
     setListJob(value);
   };
-
-  /**
-   * Hàm thêm mới công việc
-   *  Auth: NVQUY (12/05/2024)
-   */
   const handleCreateJob = (): void => {
     // Kiểm tr dữ liệu đầu vào
     if (inputValue) {
@@ -71,37 +45,17 @@ export default function TodoList() {
         name: inputValue,
         status: false,
       };
-      // Bước 2: Push dữ liệu vào mảng (mảng này lấy từ localStorage)
       listJob.push(job);
-
-      // Bước 3: Lưu danh sách công việc lên localStorage
       saveData("jobs", listJob);
-
-      // Bước 4: Reset giá trị trong ô input
       setInputValue("");
     } else {
       setShowError(true);
     }
   };
-
-  /**
-   * Hàm xóa thông tin một công việc theo id
-   * @param id Id của công việc cần xóa
-   *  Auth: NVQUY (12/05/2024)
-   */
   const handleDelete = (id: string) => {
-    // Lọc ra những công việc có id khác với id cần xóa
     const filterJob = listJob.filter((job: JobType) => job.id !== id);
-
-    // Lưu mảng mới lên local
     saveData("jobs", filterJob);
   };
-
-  /**
-   * Hàm cập nhật trạng thái công việc
-   * @param id Id của công việc cần cập nhật
-   * Auth: NVQUY (12/05/2024)
-   */
   const handleChecked = (id: string) => {
     // Cập nhật trạng thái của công việc theo id
     // option: dùng hàm findIndex lấy ra vị trí công việc và cập nhật nó
@@ -112,30 +66,12 @@ export default function TodoList() {
       }
       return job;
     });
-
-    // Lưu dữ liệu mới nhất lên localStorage
     saveData("jobs", updateJobs);
   };
-
-  /**
-   * Hàm đếm số lượng công việc đã hoàn thành
-   * @returns Số lượng công việc đã hoàn thành
-   * Auth: NVQUY (12/05/2024)
-   */
   const totalCountJobSuccess = () => {
-    // Lọc ra những công việc có status là true
-
-    // const filterJobSuccess = listJob.filter((job: JobType) => {
-    //   return job.status === true;
-    // });
-
     const filterJobSuccess = listJob.filter((job: JobType) => job.status);
-
-    // Trả về độ dài của mảng trên
     return filterJobSuccess.length;
   };
-
-  // Tìm kiếm công việc và fill lên input
   const handleUpdateName = (id: string) => {
     const findJob = listJob.find((job: JobType) => job.id === id);
     if (findJob) {
@@ -144,7 +80,6 @@ export default function TodoList() {
       setIdUpdate(id);
     }
   };
-
   // Cập nhật lại giá trị và thông tin công việc mới lên localStorage
   const handleSaveUpdate = () => {
     const updateJobs = listJob.map((job: JobType) => {
@@ -160,7 +95,6 @@ export default function TodoList() {
     setInputValue("");
     setTypeButton("add");
   };
-
   return (
     <>
       <div className="main">
@@ -189,8 +123,6 @@ export default function TodoList() {
               )}
             </div>
             {
-              // showError === true ? ( <p className="error">Tên công việc không được để trống.</p>) : (<></>)
-              // showError ? ( <p className="error">Tên công việc không được để trống.</p>) : (<></>)
               showError && (
                 <p className="error">Tên công việc không được để trống.</p>
               )
